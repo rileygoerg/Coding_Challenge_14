ebd62e82f66647a178d25fa2dde4789fa0821882
rileygoerg <rgoerg@usf.edu>
2024-07-02 19:28:17 -0400

    Add input validation for user interface.

--- js/loanPayment.js
+++ js/loanPayment.js
@@ -1,5 +1,7 @@
 // M = P[r(1+r)^n]/[(1+r)^n-1]
 // loanTime is in months
 export function calcLoan(loanPrincipal, loanRate, loanTime){
-    return (loanPrincipal * (((loanRate/1200)*(1+(loanRate/1200))^loanTime)/(((1+(loanRate/1200))^loanTime)-1)))/loanTime
+    const rate = loanRate/100
+    return (loanPrincipal * (rate*((1+rate)^loanTime)))/(((1+rate)^loanTime)-1)
 }
+
--- js/main.js
+++ js/main.js
@@ -3,25 +3,40 @@ import { calcInterest } from "./interestRate.js";
 import { calcLoan } from "./loanPayment.js";
 import { calcInvestment } from "./investmentReturn.js";
 
+// Interest 
 document.getElementById("calculateInterest").addEventListener("click", function(){
     const interestPrincipal = parseFloat(document.getElementById("interestPrincipal").value);
     const interestRate = parseFloat(document.getElementById("interestRate").value);
     const interestTime = parseFloat(document.getElementById("interestTime").value);
     const interest = calcInterest(interestPrincipal, interestRate, interestTime);
-    document.getElementById("totalInterest").innerText = `Total Interest Charged: $${interest.toFixed(2)}`
+    if (isNaN(interestPrincipal) || isNaN(interestRate) || isNaN(interestTime)){
+        alert('Please fill out all fields for the interest calculation.')
+    } else {
+        document.getElementById("totalInterest").innerText = `Total Loan Payment: $${interest.toFixed(2)}`
+    }
 })
+// Loan
 document.getElementById("calculateLoan").addEventListener("click", function(){
     const loanPrincipal = parseFloat(document.getElementById("loanPrincipal").value);
     const loanRate = parseFloat(document.getElementById("loanRate").value);
     const loanTime = parseFloat(document.getElementById("loanTime").value);
     const loan = calcLoan(loanPrincipal, loanRate, loanTime);
-    document.getElementById("totalLoan").innerText = `Monthly Loan Payment: $${loan.toFixed(2)}`
+    if (isNaN(loanPrincipal) || isNaN(loanRate) || isNaN(loanTime)){
+        alert('Please fill out all fields for the loan calculation.')
+    } else {
+        document.getElementById("totalLoan").innerText = `Monthly Loan Payment: $${loan.toFixed(2)}`
+    }
 })
+// Investments
 document.getElementById("calculateInvestment").addEventListener("click", function(){
     const investmentPrincipal = parseFloat(document.getElementById("investmentPrincipal").value);
     const investmentRate = parseFloat(document.getElementById("investmentRate").value);
     const investmentCompound = parseFloat(document.getElementById("investmentCompound").value);
     const investmentTime = parseFloat(document.getElementById("investmentTime").value);
     const investment = calcInvestment(investmentPrincipal, investmentRate, investmentCompound, investmentTime);
-    document.getElementById("totalInvestment").innerText = `Total Value of Investment: ${investment.toFixed(2)}`
+    if (isNaN(investmentPrincipal) || isNaN(investmentRate) || isNaN(investmentCompound) || isNaN(investmentTime)){
+        alert('Please fill out all fields for the investment calculation.')
+    } else {
+        document.getElementById("totalInvestment").innerText = `Total Value of Investment: ${investment.toFixed(2)}`
+    }
 })
