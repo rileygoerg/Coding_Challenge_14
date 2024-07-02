af233650397b7b4335194d0757ac16577aa81bf1
rileygoerg <rgoerg@usf.edu>
2024-07-02 17:01:27 -0400

    Implement interest rate calculation module.

--- js/interestRate.js
+++ js/interestRate.js
@@ -1,4 +1,4 @@
 // Interest = Principal * Rate * Time
-export function calcInterest(){principal, rate, time 
+export function calcInterest(principal, rate, time){
     return principal * (rate/100) * time
 }
